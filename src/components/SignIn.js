3233be4acaea4a3a5cb581c78ee3a29753223298
Samiran Roy <samiranroy0407@gmail.com>
2021-06-02 21:54:53 +0530

    Login values

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 
 class SignIn extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      password: '',
+      email: '',
+    };
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const { email, password } = this.state;
+
+    console.log(email);
+    console.log(password);
+  };
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
         <div className="field">
-          <input type="email" placeholder="Email" required />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            onChange={this.handleEmailChange}
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" required />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            onChange={this.handlePasswordChange}
+          />
         </div>
         <div className="field">
-          <button>Log In</button>
+          <button onClick={this.handleFormSubmit}>Log In</button>
         </div>
       </form>
     );
